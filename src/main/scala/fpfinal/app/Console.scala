3e764f83a55e93234e3dec21cc31428685d96382
Leandro Galvan <leusgalvan@gmail.com>
2021-03-16 18:05:13 -0300

    Make Console methods return IO

--- src/main/scala/fpfinal/app/Console.scala
+++ src/main/scala/fpfinal/app/Console.scala
@@ -1,29 +1,29 @@
 package fpfinal.app
 
-import cats._
-import cats.data.Validated.{Invalid, Valid}
 import cats.implicits._
-import fpfinal.app.Configuration.IsValid
 
 import scala.io.StdIn
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 trait Console {
   val console: Service
 
   trait Service {
-    def readLine(msg: String): String
-    def printLine(line: String): Unit
-    def readDouble(msg: String): Option[Double]
+    def readLine(msg: String): IO[String]
+    def printLine(line: String): IO[Unit]
+    def readDouble(msg: String): IO[Option[Double]]
   }
 }
 
 trait LiveConsole extends Console {
   override val console: Service = new Service {
-    override def readLine(msg: String): String = StdIn.readLine(msg)
-    override def printLine(line: String): Unit = println(line)
-    override def readDouble(msg: String): Option[Double] = {
-      Try(readLine(msg).toDouble).toOption
-    }
+    override def readLine(msg: String): IO[String] =
+      IO(StdIn.readLine(msg))
+
+    override def printLine(line: String): IO[Unit] =
+      IO(println(line))
+
+    override def readDouble(msg: String): IO[Option[Double]] =
+      readLine(msg).map(line => Try(line.toDouble).toOption)
   }
 }
