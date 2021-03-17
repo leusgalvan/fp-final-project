97e9fff7fc1dff1a61bc7d62a3889a7c401e2060
Leandro Galvan <leusgalvan@gmail.com>
2021-03-17 18:57:37 -0300

    Implement Main

--- src/main/scala/fpfinal/Main.scala
+++ src/main/scala/fpfinal/Main.scala
@@ -2,11 +2,15 @@ package fpfinal
 
 import fpfinal.app.App
 import fpfinal.app.Configuration._
-import cats._
-import cats.data._
-import cats.implicits._
+import fpfinal.model.Person
+import fpfinal.service.ExpenseService.ExpenseState
+import fpfinal.service.PersonService.PersonState
+
 object Main {
   def main(args: Array[String]): Unit = {
-    App.run().run(liveEnv).runA(AppState())
+    App
+      .run()
+      .run(liveEnv)
+      .runA(AppState(ExpenseState(Nil), PersonState(Map.empty[String, Person])))
   }
 }
