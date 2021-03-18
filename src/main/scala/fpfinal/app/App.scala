6c71408807bbfe85a7fe8a811662b64fa0ae7cd4
Leandro Galvan <leusgalvan@gmail.com>
2021-03-18 16:39:48 -0300

    Fix error handling

--- src/main/scala/fpfinal/app/App.scala
+++ src/main/scala/fpfinal/app/App.scala
@@ -27,7 +27,7 @@ object App {
     } yield ()
   }
 
-  def readCommand(): AppOp[Int] = {
+  def readCommandNumber(): AppOp[Int] = {
     for {
       env <- readEnv
       option <- env.console.readLine("Your option: ").toAppOp
@@ -40,17 +40,22 @@ object App {
       for {
         env <- readEnv
         _ <- printOptions
-        commandNumber <- readCommand()
-        command = env.controller.getCommandByNumber(commandNumber)
-        successMsg <- command.fold(
-          ME.raiseError[SuccessMsg]("Command not found")
-        )(_.execute())
+        commandNumber <- readCommandNumber()
+        command <- ME.fromOption(
+          env.controller.getCommandByNumber(commandNumber),
+          "Command not found"
+        )
+        successMsg <- command.execute()
         _ <- env.console.printLine(successMsg).toAppOp
-        _ <- if (command.exists(_.isExit)) ME.unit else loop
+        _ <- if (command.isExit) ME.unit else loop
       } yield ()
 
     ME.handleErrorWith(loop) { errorMessage =>
-      readEnv.map(_.console.printLine(errorMessage)) >> run()
+      for {
+        env <- readEnv
+        _ <- env.console.printLine(errorMessage).toAppOp
+        _ <- run
+      } yield ()
     }
   }
 }
--- src/test/scala/fpfinal/app/AppSpec.scala
+++ src/test/scala/fpfinal/app/AppSpec.scala
@@ -13,24 +13,27 @@ class AppSpec extends AnyFunSuite with Matchers {
       override val commands: Map[Int, Command] =
         Map(0 -> AddPersonCommand, 1 -> AddExpenseCommand, 2 -> ExitCommand)
       override var linesToRead: List[String] = List(
-        "0", // The command number (add expense)
-        "Leandro", // The name of the person
-        "1", // The command number (add expense)
-        "Leandro", // The payer
-        "2000.00", // The amount
-        "Martin", // The first participant
-        "Susan", // The second participant
-        "END", // No more participants
-        "2" // The command number (exit)
+//        "0", // The command number (add expense)
+//        "Leandro", // The name of the person
+//        "1", // The command number (add expense)
+//        "Leandro", // The payer
+//        "2000.00", // The amount
+//        "Martin", // The first participant
+//        "Susan", // The second participant
+//        "END", // No more participants
+//        "2" // The command number (exit)
+        "4" // The command number (exit)
       )
     }
 
-    App
+    val x = App
       .run()
       .run(fakeEnv)
       .run(AppState(ExpenseState(Nil), PersonState(Map.empty)))
       .value
       .run
+
+    println(x)
 
     println(fakeEnv.linesWritten.mkString("\n"))
     fakeEnv.peopleSearched shouldBe 3
