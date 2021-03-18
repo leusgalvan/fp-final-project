c4b5804374f087989f6a24362f47dc2752de11ca
Leandro Galvan <leusgalvan@gmail.com>
2021-03-18 11:01:16 -0300

    Add Person validations

--- src/main/scala/fpfinal/app/Validations.scala
+++ src/main/scala/fpfinal/app/Validations.scala
@@ -9,11 +9,14 @@ object Validations {
     Validated.fromOption(s.toDoubleOption, NonEmptyChain("Invalid double"))
 
   def positive(x: Double): IsValid[Double] =
-    Validated.condNec(x > 0, x, s"$x is not positive")
+    Validated.condNec(x > 0, x, s"$x should be positive")
 
   def nonEmptyList[A](list: List[A]): IsValid[NonEmptyList[A]] =
-    Validated.fromOption(list.toNel, NonEmptyChain("List is empty"))
+    Validated.fromOption(list.toNel, NonEmptyChain("List should be non-empty"))
 
   def nonEmptyString(s: String): IsValid[String] =
-    Validated.condNec(s.nonEmpty, s, "String is empty")
+    Validated.condNec(s.nonEmpty, s, "String should be non-empty")
+
+  def allLetters(s: String): IsValid[String] =
+    Validated.condNec(s.forall(_.isLetter), s, "String should be all letters")
 }
--- src/main/scala/fpfinal/model/Person.scala
+++ src/main/scala/fpfinal/model/Person.scala
@@ -1,9 +1,13 @@
 package fpfinal.model
 
 import fpfinal.app.Configuration.IsValid
+import fpfinal.app.Validations._
+import cats._
+import cats.implicits._
 
 case class Person private (name: String)
 
 object Person {
-  def createPerson(name: String): IsValid[Person] = ???
+  def createPerson(name: String): IsValid[Person] =
+    (allLetters(name), nonEmptyString(name)).mapN((_, _) => Person(name))
 }
