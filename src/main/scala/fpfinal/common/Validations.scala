9e99e405154992b4d34dee7731c5c7541b7e6878
Leandro Galvan <leusgalvan@gmail.com>
2021-03-23 19:50:02 -0300

    Change some messaging

--- src/main/scala/fpfinal/common/Validations.scala
+++ src/main/scala/fpfinal/common/Validations.scala
@@ -18,12 +18,12 @@ object Validations {
     Validated.fromOption(s.toDoubleOption, NonEmptyChain("Invalid double"))
 
   def nonNegative(x: Double): IsValid[Double] =
-    Validated.condNec(x >= 0, x, s"$x should be nonnegative")
+    Validated.condNec(x >= 0, x, s"Double should be nonnegative")
 
   def nonEmptySet[A: Order](list: List[A]): IsValid[NonEmptySet[A]] =
     Validated.fromOption(
       NonEmptySet.fromSet(SortedSet.from(list)(Order[A].toOrdering)),
-      NonEmptyChain("Elements list should be non-empty")
+      NonEmptyChain("List should be non-empty")
     )
 
   def nonEmptyString(s: String): IsValid[String] =
