e58eee3fb21f7501f8e5ac5704511f917b3da0a6
Leandro Galvan <leusgalvan@gmail.com>
2021-03-24 12:48:23 -0300

    Add generators exercises

--- src/test/scala/fpfinal/Generators.scala
+++ src/test/scala/fpfinal/Generators.scala
@@ -12,28 +12,28 @@ import fpfinal.service.PersonService.{PersonOp, PersonState}
 import org.scalacheck.{Arbitrary, Gen}
 
 trait Generators {
-  implicit val personArb: Arbitrary[Person] = Arbitrary {
-    for {
-      n <- Gen.choose(0, 32)
-      name <- Gen.stringOfN(n, Gen.alphaChar)
-    } yield Person.unsafeCreate(name)
-  }
+
+  /**
+    * TODO: implement an arbitrary of Person.
+    *
+    * You can use Person.unsafeCreate as long as you take
+    * care of only producing valid values (check out
+    * the constraints in Person.create) .
+    */
+  implicit val personArb: Arbitrary[Person] = ???
 
   implicit val moneyArb: Arbitrary[Money] = Arbitrary {
     Gen.choose(1, 1e9.toInt).map(Money.unsafeCreate)
   }
 
+  /**
+    * TODO: Use the provided arbitraries and the Expense.unsafeCreate method
+    * to create an instance of Arbitrary[Expense]
+    */
   implicit def expenseArb(implicit
       arbPerson: Arbitrary[Person],
       arbMoney: Arbitrary[Money]
-  ): Arbitrary[Expense] =
-    Arbitrary {
-      for {
-        person <- arbPerson.arbitrary
-        money <- arbMoney.arbitrary
-        participants <- Gen.nonEmptyListOf(arbPerson.arbitrary)
-      } yield Expense.unsafeCreate(person, money, participants)
-    }
+  ): Arbitrary[Expense] = ???
 
   implicit val payeeDebtArb: Arbitrary[DebtByPayee] = Arbitrary {
     Gen
@@ -91,15 +91,17 @@ trait Generators {
       } yield AppState(es, ps)
     }
 
+  /**
+    * TODO: implement an arbitrary of PersonOp[A].
+    *
+    * One possible implementation is to create a State
+    * whose run function ignores the current state and just
+    * sets the state and value to random values.
+    */
   implicit def personOpArb[A](implicit
       arbA: Arbitrary[A],
       arbPersonState: Arbitrary[PersonState]
-  ): Arbitrary[PersonOp[A]] = {
-    Arbitrary(for {
-      a <- arbA.arbitrary
-      ps <- arbPersonState.arbitrary
-    } yield State((_: PersonState) => (ps, a)))
-  }
+  ): Arbitrary[PersonOp[A]] = ???
 
   implicit def isValidArb[A](implicit
       arbA: Arbitrary[A]
