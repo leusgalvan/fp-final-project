fd30c656f764118976bae60f27a87bd5bb936605
Leandro Galvan <leusgalvan@gmail.com>
2022-05-07 19:34:26 -0300

    Add comments for the DebtByPayee class

--- src/main/scala/fpfinal/model/DebtByPayee.scala
+++ src/main/scala/fpfinal/model/DebtByPayee.scala
@@ -3,6 +3,21 @@ package fpfinal.model
 import cats._
 import cats.implicits._
 
+/**
+ * This class holds information about how much money is owed by each Person.
+ * It does not contain information about the person the money is owed to, only
+ * about the people who owes (i.e. the 'payees').
+ *
+ * For example, if Alan owes 10 dollars and Betty owes 50:
+ *
+ * allPayees() == List(Alan, Betty)
+ *
+ * debtForPayee(Alan) == Some($10.00)
+ * debtForPayee(Betty) == Some($50.00)
+ * debtForPayee(Charly) == None
+ *
+ * @param debtByPayee a map containing each payee along with their debt
+ */
 class DebtByPayee private (val debtByPayee: Map[Person, Money]) {
 
   /**
@@ -17,15 +32,29 @@ class DebtByPayee private (val debtByPayee: Map[Person, Money]) {
 }
 
 object DebtByPayee {
+  /**
+   * Creates a DebtByPayee instance using the information contained in the map.
+   * Should be only used in tests.
+   *
+   * @param debtByPayee a map containing each payee along with their debt
+   */
   def unsafeCreate(debtByPayee: Map[Person, Money]): DebtByPayee =
     new DebtByPayee(debtByPayee)
 
   /**
     * TODO: Create a DebtByPayee instance using the information from this Expense.
     * Each participant should get the same debt to the payer.
+    * For simplicity we don't care about losing cents. For example, dividing 1 dollar
+    * among 3 participants should yield 33 cents of debt for each participant.
     */
   def fromExpense(expense: Expense): DebtByPayee = ???
 
+  /**
+   * Creates an instance of DebtByPayee with exactly one payee and their debt.
+   *
+   * @param person the payee (person who owes)
+   * @param money the amount owed by the payee
+   */
   def singleton(person: Person, money: Money): DebtByPayee = new DebtByPayee(Map(person -> money))
 
   /**
