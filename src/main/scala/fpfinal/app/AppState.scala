7377a7f238a8641948a0e8a5050ba5fb5a00b5e2
Leandro Galvan <leusgalvan@gmail.com>
2022-05-07 22:57:52 -0300

    Add comments to Console and AppState

--- src/main/scala/fpfinal/app/AppState.scala
+++ src/main/scala/fpfinal/app/AppState.scala
@@ -5,8 +5,18 @@ import cats.implicits._
 import fpfinal.service.ExpenseService.ExpenseState
 import fpfinal.service.PersonService.PersonState
 
+/**
+ * The application state is the combination of the ExpenseState and the PersonState.
+ *
+ * That is, it contains information about what expenses have been added and
+ * what people have been added so far.
+ */
 case class AppState(expenseState: ExpenseState, personState: PersonState)
+
 object AppState {
+  /**
+   * An application state without expenses nor people added.
+   */
   def empty: AppState = AppState(ExpenseState.empty, PersonState.empty)
 
   implicit def eqAppState(implicit
--- src/main/scala/fpfinal/app/Console.scala
+++ src/main/scala/fpfinal/app/Console.scala
@@ -4,11 +4,24 @@ import fpfinal.common.IO
 
 import scala.io.StdIn
 
+/**
+ * A simple implementation of a console that writes and reads
+ * to and from standard input.
+ */
 trait Console {
   val console: Service
 
   trait Service {
+    /**
+     * Reads a line from standard input.
+     *
+     * @param msg a message to show in the console before reading the line (such as 'Please enter a name: ')
+     */
     def readLine(msg: String): IO[String]
+
+    /**
+     * Prints a line to standard output.
+     */
     def printLine(line: String): IO[Unit]
   }
 }
