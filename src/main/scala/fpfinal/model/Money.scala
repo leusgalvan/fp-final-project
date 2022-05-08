19be6c938ff6b7b5902aed3d2ac08a7f3a0eb016
Leandro Galvan <leusgalvan@gmail.com>
2022-05-07 21:53:14 -0300

    Add comments to Money

--- src/main/scala/fpfinal/model/Money.scala
+++ src/main/scala/fpfinal/model/Money.scala
@@ -8,23 +8,65 @@ import fpfinal.model.Money.showMoney
 
 import scala.util.Try
 
+/**
+ * Simple class for representing money amounts.
+ *
+ * It operates on cents, in order to keep a better precision.
+ *
+ * @param _cents the amount in cents
+ */
 class Money private (_cents: Int) {
+  /**
+   * @return the amount expressed in cents
+   */
   def cents: Int = _cents
+
+  /**
+   * @return the amount expressed in dollars
+   */
   def dollars: Double = _cents / 100.0
+
+  /**
+   * Adds this money amount to another money amount,
+   * keeping precision up to the cents.
+   */
   def plus(other: Money): Money = new Money(_cents + other.cents)
+
+  /**
+   * Substracts a money amount from this money amount,
+   * keeping precision up to the cents.
+   */
   def minus(other: Money): Money = new Money(_cents - other.cents)
+
+  /**
+   * Multiplies this money amount by a constant factor,
+   * keeping precision up to the cents.
+   */
   def times(n: Int): Money = new Money(_cents * n)
 
   /**
     * TODO: Implement division by an integer. When n is 0, the result should be None.
     * Otherwise, it should be a Some.
+    *
+    * For simplicity we don't care about losing cents. For example, dividing 1 dollar
+    * by 3 should yield 33 cents.
     */
   def divideBy(n: Int): Option[Money] = ???
 
+  /**
+   * @return a string representation of this money amount in dollars, with two decimal places
+   *         and a dollar sign (e.g. $45.33)
+   */
   override def toString: String = showMoney.show(this)
 }
 
 object Money {
+  /**
+   * Creates an instance of Money without performing any validations.
+   * Should only be used in tests.
+   *
+   * @param cents the amount expressed in cents
+   */
   def unsafeCreate(cents: Int): Money = new Money(cents)
 
   val zero: Money = new Money(0)
@@ -45,7 +87,7 @@ object Money {
 
   /**
     * TODO: Implement and instance of Order for Money that compares its cents.
-    * We should we able to use any Order[Int] we provide as an implicit parameter.
+    * Use the given Order instance for comparing any Int values.
     */
-  implicit def orderMoney = ???
+  implicit def orderMoney(implicit orderInt: Order[Int]): Order[Money] = ???
 }
