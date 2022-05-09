77c274e80e67d498558fc703ba5c7d5f05594df0
Leandro Galvan <leusgalvan@gmail.com>
2022-05-08 22:50:38 -0300

    Add error handling to IO; finish Syntax comments

--- src/main/scala/fpfinal/app/Syntax.scala
+++ src/main/scala/fpfinal/app/Syntax.scala
@@ -15,12 +15,19 @@ object Syntax {
     /**
      * Lifts an IO[A] into an AppOp[A].
      *
-     * An AppOp[A] has three layers, namely (from inner to outer):
-     * - An EitherT for error handling: in our case
-     * @return
+     * The important bit is to lift the IO into EitherT correctly. By using attempt,
+     * we lift the errors into value space, so we can represent them using Left
+     * instead of throwing them. We can then use Right to represent successful values.
+     * Notice that the error type for attempt is Throwable but we need String, so
+     * we have to make the conversion.
+     *
+     * This computation does not alter the state and does not read from the environment,
+     * so all that is left is to lift it, first to a StateT and then to a ReaderT,
+     * by using the corresponding liftF methods.
      */
     def toAppOp: AppOp[A] = {
-      val errorOr: ErrorOr[A] = EitherT.liftF(fa)
+      val attemptIO: IO[Either[Error, A]] = fa.attempt.map(_.leftMap(_.getMessage))
+      val errorOr: ErrorOr[A] = EitherT(attemptIO)
       val st: St[A] = StateT.liftF(errorOr)
       ReaderT.liftF(st)
     }
@@ -30,28 +37,54 @@ object Syntax {
     /**
       * TODO: Translate between a PersonOp and an AppOp.
       *
-      * Make sure the resulting AppState contains the PersonState
-      * from this PersonOp.
+      * Read the description of toAppOp in ExpenseOps carefully, as this implementation should be
+      * really similar.
       */
     def toAppOp: AppOp[A] = ???
   }
 
   implicit class ValidOps[A](fa: IsValid[A]) {
+    /**
+     * Lifts an IsValid[A] into an AppOp[A].
+     *
+     * We need to turn the IsValid into an Either first, so we can lift it to EitherT.
+     * Since we may have many errors arising from validations, we need to merge them
+     * and produce a single message that will show them all.
+     *
+     * Once we do that, the rest is easy. This value does not really need to read from the environment
+     * or alter the state so we can just use liftF to lift it into StateT and ReaderT.
+     */
     def toAppOp: AppOp[A] = {
-      val errorOr: ErrorOr[A] = {
-        EitherT.fromEither(
-          fa.toEither
-            .leftMap { e =>
-              s"""Errors: ${e.mkString_("[", ", ", "]")}"""
-            }
-        )
+      val mergedValidations: Either[Error, A] = fa.toEither.leftMap { errors =>
+        s"""Errors: ${errors.mkString_("[", ", ", "]")}"""
       }
+      val errorOr: ErrorOr[A] = EitherT.fromEither(mergedValidations)
       val st: St[A] = StateT.liftF(errorOr)
       ReaderT.liftF(st)
     }
   }
 
   implicit class ExpenseOps[A](fa: ExpenseOp[A]) {
+    /**
+     * Lifts an ExpenseOp[A] into an AppOp[A].
+     *
+     * ExpenseOp[A] is an alias for State[ExpenseState, A]. When a value of this type is run,
+     * it should not produce exceptions. Furthermore, the type does not encode any information
+     * about errors. So we can assume that once we have the A we can lift it to EitherT via pure.
+     *
+     * The interesting part is in the the StateT layer. We have an ExpenseState and we want to update
+     * the ApplicationState with it. We have to be careful not to override the whole application state, though, because
+     * we could accidentally lose values!
+     *
+     * The correct way to update the state is:
+     * - Use the current expense state of the application to run our ExpenseOp, so whatever modification we do
+     *   is done on top of what we already had.
+     * - Once we have our new expense state, update the current application state with it so the
+     *   changes are reflected.
+     *
+     * The last step is easy. We don't need to read from the Environment so we can just lift the value
+     * into the ReaderT by using liftF.
+     */
     def toAppOp: AppOp[A] = {
       val st: St[A] = StateT { appState =>
         val (faS, faA) = fa.run(appState.expenseState).value
@@ -68,7 +101,7 @@ object Syntax {
      * and an initial state, which holds the initial expenses and people
      * (for example the empty state with no people nor expenses).
      *
-     * @return an either with Left indicatin an error, or Right providing a tuple with the resulting
+     * @return an either with Left indicating an error, or Right providing a tuple with the resulting
      *         state and return value of the application
      */
     def unsafeRunApp(
--- src/test/scala/fpfinal/common/IOSpec.scala
+++ src/test/scala/fpfinal/common/IOSpec.scala
@@ -2,11 +2,14 @@ package fpfinal.common
 
 import cats._
 import cats.implicits._
-import cats.laws.discipline.MonadTests
+import cats.laws.discipline.{MonadErrorTests, MonadTests}
 import fpfinal.Generators
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.prop.Configuration
 import org.typelevel.discipline.scalatest.FunSuiteDiscipline
+import IO._
+
+import scala.util.{Failure, Try}
 
 class IOSpec
     extends AnyFunSuite
@@ -17,5 +20,26 @@ class IOSpec
   implicit def eqIO[A: Eq]: Eq[IO[A]] =
     Eq.instance((io1, io2) => io1.run eqv io2.run)
 
-  checkAll("Monad[IO]", MonadTests[IO].monad[Int, Int, Int])
+  implicit val eqThrowable: Eq[Throwable] =
+    Eq.instance((t1, t2) => t1.getMessage == t2.getMessage)
+
+  test("Running a suspended exception throws") {
+    val e = new Exception("boom")
+    val ioa: IO[Int] = IO.suspend(throw e)
+    Try(ioa.run) eqv Failure(e)
+  }
+
+  test("Attempting and running a suspended exception yields a Left") {
+    val e = new Exception("boom")
+    val ioa: IO[Int] = IO.suspend(throw e)
+    ioa.attempt.run eqv Left(e)
+  }
+
+  test("Attempting and running an IO that throws yields a Left") {
+    val e = new Exception("boom")
+    val ioa: IO[Int] = IO.raiseError(e)
+    ioa.attempt.run eqv Left(e)
+  }
+
+  checkAll("MonadError[IO]", MonadErrorTests[IO, Throwable].monad[Int, Int, Int])
 }
